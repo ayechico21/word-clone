06a19fa25cfae74d2404664c147cb98f4557777e
Maneesh Cheema <ayechico21@gmail.com>
2023-04-19 19:08:44 +0530

    feat: exer 1

---
+++ src/components/InputGuess/InputGuess.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function InputGuess() {
+  const [guess, setGuess] = useState("");
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(guess);
+    setGuess("");
+  };
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleFormSubmit}>
+      <label className="guess-input" htmlFor="guess-input">
+        Enter Guess:
+      </label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+        pattern="[A-Z]{5}"
+        title="Enter 5 character word"
+      ></input>
+    </form>
+  );
+}
+
+export default InputGuess;
